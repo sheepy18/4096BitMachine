91717895afd60c253ec537db380057e017d823f2
DESKTOP-5CUBI1D\Sheepy <Sheepy@gmx.net>
2018-12-28 00:33:43 +0100

    sample class and bella ciao example

--- code/code/musical.cpp
+++ code/code/musical.cpp
@@ -3,26 +3,26 @@
 
 namespace musical
 {
-  Tone::MilliSec Tone::quarterLength = 1000;
+  float Tone::quarterLength = 1;
 
   void Tone::play( uint8_t pin )
   {
-    playForSec( noteLength, pin );
+    playForSec( noteLength * quarterLength, pin );
   }
   
   void Tone::buildFrequency( uint8_t pin, unsigned long wait )
   {
-    digitalWrite( pin, HIGH );
+    digitalWrite( pin, amp );
     delay( wait / 2 );
     digitalWrite( pin, LOW );
     delay( wait / 2 );
   }
   
-  void Tone::playForSec( MilliSec maxS, uint8_t pin )
+  void Tone::playForSec( const MilliSec maxS, const uint8_t pin )
   {
-     unsigned long wait = ( 1 / freq) * 1000;
+     const unsigned long wait = ( 1 / freq) * 1000;
 
-    for( unsigned long i = 0; i <= maxS; i = i + wait )
+    for( unsigned long i = 0; i <= maxS - interrupt; i = i + wait )
     {
       buildFrequency( pin, wait );
     }
@@ -35,5 +35,84 @@ namespace musical
     amp = am;
     interrupt = in;
     noteLength = nLength;
+  }
+
+  void Sample::play()
+  {
+    loadTones();
+    for( uint8_t i = 0; i < 10 && i < maxPosTones; ++i)
+    {
+      buff[i].play( pin );
+    }
+    if( song[currentPos] != '\0') 
+    {
+      play();
+    }
+  }
+
+  unsigned long exp10( uint8_t e )
+  {
+    if( e == 0)
+      return 1;
+    unsigned long f = 10;
+    for( uint8_t i = 0; i < e; ++i)
+      f *= 10;
+    return f;
+  }
+
+  void Sample::loadTones()
+  {
+    uint8_t t = 0;
+
+    while( t < 10 && song[currentPos] !=  '\0')
+    {
+      if( song[currentPos] == '(' )
+      {
+         uint8_t j = 0;
+         float freq = 0.f;
+         while( song[currentPos + j + 1] != '.')
+            ++j;
+            
+         if( j == 3)
+            freq += ((song[currentPos + 1] - '0') * 100) + ((song[currentPos + 2] - '0') * 10 ) + (song[currentPos + 3] - '0') - 1;
+         if( j == 2)
+            freq += ((song[currentPos + 1] - '0') * 10 ) + (song[currentPos + 2] - '0');
+         else
+            freq += (song[currentPos + 1] - '0');
+        
+         currentPos += j + 1;
+         freq += (((song[currentPos + 1] - '0') * 10 + (song[currentPos + 2] - '0'))) / 100.f;
+
+         currentPos += 3;
+         unsigned long noteLength = 0;
+         switch( song[ currentPos + 1] )
+         {
+            case '1':
+              noteLength = 1000 * 4;
+              break;
+            case '2':
+              noteLength = 1000 * 2;
+              break;
+            case '3':
+              noteLength = 1000;
+              break;
+            case '4':
+              noteLength = 500;
+              break;
+            case '5':
+              noteLength = 250;
+              break;
+            case '6':
+              noteLength = 125;
+              break;
+         }
+
+         buff[ t ].change( (freq <= 0.5) ? 1 : freq, (freq <= 0.5) ? 0 : 1,  (song[ currentPos + 2] == '0')? 0 : 50, noteLength );
+         
+         ++t;
+      }
+      ++currentPos;
+    }
+    maxPosTones = t;
   }
 }
--- code/code/musical.h
+++ code/code/musical.h
@@ -14,13 +14,20 @@ namespace musical
       using MilliSec = unsigned long;
     
     public:
-      static MilliSec quarterLength;
+    
+      friend class Sample;
+      static float quarterLength;
     
       Tone() : freq{ 0 }, amp{ 0 }, interrupt{ 20 }, noteLength{ 1 } {}
       Tone( Hz freqArg ) : freq{ freqArg }, amp{ 1 }, interrupt{ 20 } {}
       
       void change( const Hz fr, const Volt am, const MilliSec in, MilliSec nLength );
-      void play( uint8_t pin );
+      void play( const uint8_t pin );
+
+      Hz getFreq() { return freq; }
+      Volt getAmp() { return amp; }
+      MilliSec getNoteLength() { return noteLength; }
+      MilliSec getInterrupt() { return interrupt; }
       
     private:
       Hz freq;
@@ -28,9 +35,28 @@ namespace musical
       MilliSec noteLength; 
       MilliSec interrupt; 
 
-      void playForSec( MilliSec MaxS, uint8_t pin );
-      void buildFrequency( uint8_t pin, unsigned long wait );  
-  }; 
+      void playForSec( const MilliSec MaxS, const uint8_t pin );
+      void buildFrequency( const uint8_t pin, const unsigned long wait );  
+  };
+
+  class Sample
+  {
+    public:
+      Sample( const char* songArg, const uint8_t pinArg ) : song{ songArg }, pin{ pinArg } { pinMode( pin, OUTPUT ); }    
+      void play();
+      
+      Tone* getBuffer(){ return buff; }
+      uint8_t getMaxPosTones() { return maxPosTones; }
+
+    private:
+      const char* song;
+      const uint8_t pin;      
+      unsigned long currentPos;
+      uint8_t maxPosTones;
+      Tone buff[ 10 ];
+
+      void loadTones();      
+  };
 }
 
 #endif
